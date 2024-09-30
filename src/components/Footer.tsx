520d9d8560e428d53231f7c5772f3d630c022ad0
Shivansh-22866 <shivanshp0418@gmail.com>
2024-09-30 23:44:24 +0530

    a

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='mt-12 text-white/70 py-8 container mx-auto border-t border-gray-700 pt-4 flex justify-center items-center'>
         <h1 className='text-2xl font-bold'>Shivansh Pandey</h1>
 
-        <div className='flex space-x-6 mt-4'>
+        <div className='flex space-x-6 mx-8 items-center justify-center'>
             <a href="https://www.linkedin.com/in/shivansh-pandey-5a6319282" className='hover:text-gray-300'>
                 <FaLinkedin size={24} />
             </a>
