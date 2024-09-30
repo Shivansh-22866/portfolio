f8978a1fe4643857cd1de6d8cb2c77128ec57b13
Shivansh-22866 <shivanshp0418@gmail.com>
2024-09-30 19:27:41 +0530

    Navbar

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Image from 'next/image'
+import cursor from '..assets/icon1.png'
+import message from '../assets/icon2.png'
+import { motion } from 'framer-motion'
+import profilepic from '../assets/profilepic.jpg'
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
