24ee04e9354e958564aa85cfdd45adda3101cd83
Shivansh-22866 <shivanshp0418@gmail.com>
2024-09-30 22:22:52 +0530

    About section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,11 +7,11 @@ import profilepic from "../assets/profilepic.jpg";
 
 const Hero = () => {
   return (
-    <div className="py-24 relative overflow-clip bg-[linear-gradient(to_bottom,#000,#2B1942_35%,#8F5C55_60%,#DBAF6E_80%)]">
-      <div className="absolute rounded-[50%] w-[3000px] h-[1300px] bg-black top-[550px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_0%,#2B1942)]"></div>
+    <div className="py-24 relative overflow-clip" style={{ background: 'linear-gradient(to bottom, #000, #2B1942 35%, #8F5C55 60%, #DBAF6E 80%)' }}>
+      <div className="absolute rounded-[50%] w-[3000px] h-[1300px] bg-black top-[550px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_70%,#2B1942)]"></div>
 
       <div className="relative">
-        <div className="text-8xl font-bold text-center">
+        <div className="sm:text-8xl text-6xl font-bold text-center">
           <h1 className="text-[#98B4CE]">Hi, I am</h1>
           <h1 className="text-[#E48A57]">Shivansh Pandey</h1>
         </div>
@@ -22,7 +22,7 @@ const Hero = () => {
             height={190}
             width={190}
             alt="cursor"
-            className=""
+            className="h-auto w-auto sm:block hidden"
             draggable="false"
           />
         </motion.div>
@@ -33,13 +33,13 @@ const Hero = () => {
             height={120}
             width={120}
             alt="cursor"
-            className=""
+            className="h-auto w-auto sm:block hidden"
             draggable="false"
           />
         </motion.div>
 
 
-      <p className="text-center text-xl max-w-[800px] mx-auto mt-8 text-white/80">
+      <p className="text-center text-xl max-w-[800px] p-4 mx-auto mt-8 text-white/80">
         I am a web developer with expertise in Next.js and React.js, focused on
         creating dynamic and responsive applications. I also have experience in
         app development with Flutter, broadening my skill set for cross-platform
